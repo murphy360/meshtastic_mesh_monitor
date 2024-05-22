cc9c7fc9af895045cf90279e8b39d08d44cea68b
Corey Murphy <c.murphy360@gmail.com>
2024-05-21 21:39:42 -0700

    source venv

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt install -y python3 python3-pip
 # Make virtual environment
 echo "Creating virtual environment..."
 python3 -m venv meshtastic-venv # Create virtual environment
-workon meshtastic-venv # Activate virtual environment
+source meshtastic-venv/bin/activate # Activate virtual environment
 
 # Install pytap2
 echo "Installing pytap2..."
