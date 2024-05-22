f3ee93826f8e6adfe1a3e644b077b1c5d6c0a65c
Corey Murphy <c.murphy360@gmail.com>
2024-05-21 21:55:42 -0700

    start script to work on reboot

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Change to the directory 
+cd ~/Software/meshtastic_mesh_monitor
+
+# Activate the virtual environment
+source meshtastic-venv/bin/activate
+
+
+# Start the Python script
+python3 mesh-monitor.py
