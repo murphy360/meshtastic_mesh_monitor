7d20c24bbf365717be0438b623fcd1d8d8fba948
Corey Murphy <c.murphy360@gmail.com>
2024-05-21 21:35:41 -0700

    added virtual environment

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ echo "Installing dependencies..."
 sudo apt update
 sudo apt install -y python3 python3-pip
 
+# Make virtual environment
+echo "Creating virtual environment..."
+python3 -m venv meshtastic-venv # Create virtual environment
+workon meshtastic-venv # Activate virtual environment
+
 # Install pytap2
 echo "Installing pytap2..."
 pip3 install --upgrade pytap2
