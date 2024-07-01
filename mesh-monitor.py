7fcef824937da5c681ba08060042c99068c6d677
Corey Murphy <c.murphy360@gmail.com>
2024-06-30 22:45:44 -0400

    Improved Reconnection Logic

--- mesh-monitor.py
+++ mesh-monitor.py
@@ -12,12 +12,14 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 localNode = ""
 sitrep = ""
 connected = False
+connect_timeout = 10
 reply_message = "Message Received"
 logging.info("Starting Mesh Monitor")
 host = '192.168.1.31' # TODO parameterize
 short_name = 'Monitor' # Overwritten in onConnection
 long_name = 'Mesh Monitor' # Overwritten in onConnection
-interface = meshtastic.tcp_interface.TCPInterface(hostname=host)
+interface = None
+sitrep = SITREP(localNode, short_name, long_name)
 
 def onConnection(interface, topic=pub.AUTO_TOPIC):
     logging.info("Connection established")
@@ -33,9 +35,9 @@ def onConnection(interface, topic=pub.AUTO_TOPIC):
     global localNode, connected, short_name, long_name, sitrep
     localNode = interface.getNode('^local')
     connected = True
-    short_name = lookup_short_name(localNode.nodeNum)
+    short_name = lookup_short_name(interface, localNode.nodeNum)
 
-    long_name = lookup_long_name(localNode.nodeNum)
+    long_name = lookup_long_name(interface, localNode.nodeNum)
     logging.info(f"\n\n \
                 **************************************************************\n\n \
                 **************************************************************\n\n \
@@ -44,18 +46,23 @@ def onConnection(interface, topic=pub.AUTO_TOPIC):
                 **************************************************************\n\n ")
 
 
-    sitrep = SITREP(localNode, short_name, long_name)
-    location = find_my_location(localNode.nodeNum)
-    send_message(f"CQ CQ CQ de {short_name} in {location}", 2, "^all")
+    sitrep.set_local_node(localNode)
+    sitrep.set_short_name(short_name)
+    sitrep.set_long_name(long_name)
+
+    location = find_my_location(interface, localNode.nodeNum)
+    send_message(interface, f"CQ CQ CQ de {short_name} in {location}", 2, "^all")
     return
 
 def on_lost_meshtastic_connection(interface):
     logging.info("Disconnected")
-    global connected
+    global connected, host
     connected = False
-    time.sleep(10)
+    logging.info("Closing Old Interface...")
     interface.close()
-    interface = meshtastic.tcp_interface.TCPInterface(hostname=host)
+    logging.info("Reconnecting...")
+    interface = connect_to_radio(host)
+    return
 
 def onReceive(packet, interface):
 
@@ -77,14 +84,22 @@ def onReceive(packet, interface):
             interface = meshtastic.tcp_interface.TCPInterface(hostname=host)
             return
         
+        #logging.info(f"Packet received: {packet}")
+
         node_num = packet['from']
-        node_short_name = lookup_short_name(node_num)
+        node_short_name = lookup_short_name(interface, node_num)
+
+        # Filter outgoing packets
+        if packet['from'] == localNode.nodeNum:
+            logging.info(f"Packet received from {node_short_name} - Outgoing packet")
+            return
         
 
         if 'decoded' in packet:
             node_of_interest = sitrep.is_packet_from_node_of_interest(interface, packet)
             new_node = sitrep.is_packet_from_new_node(interface, packet)
-            
+            #sitrep.save_packet_to_db(packet)
+
             portnum = packet['decoded']['portnum']
             short_name_string_padded = node_short_name.ljust(4) # Pad the string to 4 characters
             if len(node_short_name) == 1:
@@ -96,7 +111,7 @@ def onReceive(packet, interface):
 
             if new_node:
                 log_string += " - New node detected!"
-                send_message(f"Welcome to the Mesh {node_short_name}! I'll respond to Ping and any Direct Messages!", 0, node_num)
+                send_message(interface, f"Welcome to the Mesh {node_short_name}! I'll respond to Ping and any Direct Messages!", 0, node_num)
 
             logging.info(log_string)
 
@@ -131,12 +146,12 @@ def onReceive(packet, interface):
                         print (f"Message sent to channel {packet['channel']} from {packet['from']}")
                         #converts string to integer
                         channelId = int(packet['channel'])
-                        reply_to_message(message_string, channelId, "^all", node_num)
+                        reply_to_message(interface, message_string, channelId, "^all", node_num)
                         return
                     # If the message is broadcast to all nodes, check if we should respond  
                     elif packet['toId'] == "^all":
                         print ("Message broadcast to all nodes from {packet['from']}")
-                        reply_to_message(message_string, 0, "^all", node_num)
+                        reply_to_message(interface, message_string, 0, "^all", node_num)
                         return  
             
             elif portnum == 'POSITION_APP':
@@ -173,19 +188,27 @@ def onReceive(packet, interface):
     except KeyError as e:
         logging.error(f"Error processing packet: {e}")
 
+def connect_to_radio(host):
+    interface = None
+    try:
+        interface = meshtastic.tcp_interface.TCPInterface(hostname=host)
+    except Exception as e:
+        logging.error(f"Error connecting to interface: {e}")
+    
+    return interface
 
-def reply_to_message(message, channel, to_id, from_id):
+def reply_to_message(interface, message, channel, to_id, from_id):
     message = message.lower()
     # Check if the message is a command
     if message == "ping":
-        node_short_name = lookup_short_name(from_id)
-        local_node_short_name = lookup_short_name(localNode.nodeNum)
+        node_short_name = lookup_short_name(interface, from_id)
+        local_node_short_name = lookup_short_name(interface, localNode.nodeNum)
         location = "Unknown"
-        location = find_my_location(localNode.nodeNum)
+        location = find_my_location(interface, localNode.nodeNum)
         if location != "Unknown":
-            send_message(f"{node_short_name} de {local_node_short_name}, Pong from {location}", channel, to_id)
+            send_message(interface, f"{node_short_name} de {local_node_short_name}, Pong from {location}", channel, to_id)
         else:
-            send_message("Pong", channel, to_id)
+            send_message(interface, "Pong", channel, to_id)
         sitrep.log_message_sent("ping-pong")
         return
     elif message == "sitrep":
@@ -197,24 +220,22 @@ def reply_to_message(message, channel, to_id, from_id):
         print(f"Message not recognized: {message}. Not replying.")
         return 
 
-def lookup_short_name(node_num):
+def lookup_short_name(interface, node_num):
     for n in interface.nodes.values():
         if n["num"] == node_num:
             return n["user"]["shortName"]
     return "Unknown"
 
-def lookup_long_name(node_num):
+def lookup_long_name(interface, node_num):
     for n in interface.nodes.values():
         if n["num"] == node_num:
             return n["user"]["longName"]
     return "Unknown"
 
-def find_my_location(node_num):
+def find_my_location(interface, node_num):
     for node in interface.nodes.values():
         if node["num"] == node_num:
-            print(f"Node: {node}")
             if 'position' in node:
-                print(f"Position: {node['position']}")
                 if 'latitude' in node['position'] and 'longitude' in node['position']:
                     nodeLat = node["position"]["latitude"]
                     nodeLon = node["position"]["longitude"]
@@ -247,11 +268,11 @@ def find_my_location(node_num):
     
     return "Unknown"
     
-def send_message (message, channel, to_id):
+def send_message (interface, message, channel, to_id):
     interface.sendText(message, channelIndex=channel, destinationId=to_id)
     node_name = to_id
     if to_id != "^all":
-        node_name = lookup_short_name(to_id)
+        node_name = lookup_short_name(interface, to_id)
     logging.info(f"Packet Sent: {message} to channel {channel} and node {node_name}")
 
 pub.subscribe(onReceive, 'meshtastic.receive')
@@ -259,32 +280,22 @@ pub.subscribe(onConnection, "meshtastic.connection.established")
   # Subscribe to lost connection event
 pub.subscribe(on_lost_meshtastic_connection, "meshtastic.connection.lost")
 
-
-def is_interface_alive(interface):
-    logging.info("Checking Interface")
-    try:
-        # Try to send a dummy message to the interface
-        
-        return True
-
-    except Exception as e:
-        logging.error(f"Error checking interface: {e}")
-    
-    return False
-    
+# Main loop
 while True:
     if not connected:
-        logging.info("Not connected, trying to connect")
+        logging.info("Not connected to Radio, trying to connect")
         try:
-            interface = meshtastic.tcp_interface.TCPInterface(hostname=host)
+            interface = connect_to_radio(host)
+            if interface:
+                logging.info("Connection to Radio Established.")
+            else:
+                logging.error("Error connecting to interface: Interface is None.")
+                connect_timeout += 10
         except Exception as e:
             logging.error(f"Error connecting to interface: {e}")
             continue
     else:
-        logging.info("Connected to Radio")
+        connect_timeout = 10
+        logging.info("Connected to Radio, Sleeping...")
     
-
-    # stop 10 seconds without blocking the publisher
-    logging.info("Sleeping...")
-    time.sleep(10)
-    logging.info("Waking up...")
+    time.sleep(connect_timeout)
