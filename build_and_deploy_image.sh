c8539b9f23585c20ec790cab6500a28256d52520
Corey Murphy <c.murphy360@gmail.com>
2024-07-17 11:18:25 -0400

    auto build script

---
+++ build_and_deploy_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build the Docker image
+docker build -t meshtastic_mesh_monitor .
+
+# Change directory to ~/docker
+cd ~/docker
+
+# Run Docker Compose in detached mode
+docker compose up -d
