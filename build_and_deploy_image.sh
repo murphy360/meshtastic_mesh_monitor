8758d873ea1f58927282df46955e0ada42d67a9e
Corey Murphy <c.murphy360@gmail.com>
2024-07-24 05:58:40 -0700

    Fixing error in print

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 # git pull
-print "\n\n\n***************************************************\n"
-print "Pulling the latest changes from the repository...\n"
-print "***************************************************\n\n\n"
+printf "***************************************************\n"
+printf "Pulling the latest changes from the repository...\n"
+printf "***************************************************\n\n\n"
 git pull
 
 # Build the Docker image
-print "Building the Docker image...\n"
+print "\n\n\n***************************************************\n"
+printf "Building the Docker image...\n"
+printf "***************************************************\n\n\n"
 docker build -t meshtastic_mesh_monitor .
 
 # Change directory to ~/docker
-print "\n\n\n***************************************************\n"
-print "Changing directory to ~/docker...\n"
-print "***************************************************\n\n\n"
+printf "\n\n\n***************************************************\n"
+printf "Changing directory to ~/docker...\n"
+printf "***************************************************\n\n\n"
 cd ~/docker
 
 # Run Docker Compose in detached mode
-print "\n\n\n***************************************************\n"
-print "Running Docker Compose in detached mode...\n"
-print "***************************************************\n\n\n"
+printf "\n\n\n***************************************************\n"
+printf "Running Docker Compose in detached mode...\n"
+printf "***************************************************\n\n\n"
 docker compose up -d
