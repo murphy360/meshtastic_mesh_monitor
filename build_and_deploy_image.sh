c1949e6607c98faecda623f614993a19f7a52c23
Corey Murphy <c.murphy360@gmail.com>
2024-07-24 05:59:46 -0700

    Troubleshooting print statements

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -7,19 +7,19 @@ printf "***************************************************\n\n\n"
 git pull
 
 # Build the Docker image
-print "\n\n\n***************************************************\n"
+print "***************************************************\n"
 printf "Building the Docker image...\n"
 printf "***************************************************\n\n\n"
 docker build -t meshtastic_mesh_monitor .
 
 # Change directory to ~/docker
-printf "\n\n\n***************************************************\n"
+printf "***************************************************\n"
 printf "Changing directory to ~/docker...\n"
 printf "***************************************************\n\n\n"
 cd ~/docker
 
 # Run Docker Compose in detached mode
-printf "\n\n\n***************************************************\n"
+printf "***************************************************\n"
 printf "Running Docker Compose in detached mode...\n"
 printf "***************************************************\n\n\n"
 docker compose up -d
