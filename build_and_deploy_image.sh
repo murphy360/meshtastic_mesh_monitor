a50b8a001f713d96c2d7c69e6290d82c5a28e996
Corey Murphy <c.murphy360@gmail.com>
2024-07-24 05:55:50 -0700

    Update print statements in deploy script

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # git pull
+print "\n\n\n***************************************************\n"
 print "Pulling the latest changes from the repository...\n"
+print "***************************************************\n\n\n"
 git pull
 
 # Build the Docker image
@@ -9,12 +11,13 @@ print "Building the Docker image...\n"
 docker build -t meshtastic_mesh_monitor .
 
 # Change directory to ~/docker
+print "\n\n\n***************************************************\n"
 print "Changing directory to ~/docker...\n"
+print "***************************************************\n\n\n"
 cd ~/docker
 
 # Run Docker Compose in detached mode
-print "***************************************************"
+print "\n\n\n***************************************************\n"
 print "Running Docker Compose in detached mode...\n"
-print "***************************************************"
-
+print "***************************************************\n\n\n"
 docker compose up -d
