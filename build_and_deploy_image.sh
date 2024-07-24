7c5da635deb3dc0a86300886b0170430db54f919
Corey Murphy <c.murphy360@gmail.com>
2024-07-24 05:52:58 -0700

    Update logging in build script

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # git pull
+printf "Pulling the latest changes from the repository...\n"
 git pull
 
 # Build the Docker image
+printf "Building the Docker image...\n"
 docker build -t meshtastic_mesh_monitor .
 
 # Change directory to ~/docker
+printf "Changing directory to ~/docker...\n"
 cd ~/docker
 
 # Run Docker Compose in detached mode
+printf "***************************************************"
+printf "Running Docker Compose in detached mode...\n"
+printf "***************************************************"
+
 docker compose up -d
