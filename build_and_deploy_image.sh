9a7007609c3784d22a1aba0924aa7123b53e681f
Corey Murphy <c.murphy360@gmail.com>
2024-07-24 06:01:27 -0700

    Adding Spacing

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # git pull
-printf "***************************************************\n"
+printf "\n\n\n***************************************************\n"
 printf "Pulling the latest changes from the repository...\n"
 printf "***************************************************\n\n\n"
 git pull
 
 # Build the Docker image
-printf "***************************************************\n"
+printf "\n\n\n***************************************************\n"
 printf "Building the Docker image...\n"
 printf "***************************************************\n\n\n"
 docker build -t meshtastic_mesh_monitor .
 
 # Change directory to ~/docker
-printf "***************************************************\n"
+printf "\n\n\n***************************************************\n"
 printf "Changing directory to ~/docker...\n"
 printf "***************************************************\n\n\n"
 cd ~/docker
 
 # Run Docker Compose in detached mode
-printf "***************************************************\n"
+printf "\n\n\n***************************************************\n"
 printf "Running Docker Compose in detached mode...\n"
 printf "***************************************************\n\n\n"
 docker compose up -d
