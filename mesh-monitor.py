b7b0d14bc2013aba955ece9d099dbbcbe8241576
Corey Murphy <c.murphy360@gmail.com>
2024-12-22 11:11:14 -0800

    Initialize last routine SITREP date if not set in should_send_sitrep_after_midnight function

--- mesh-monitor.py
+++ mesh-monitor.py
@@ -342,6 +342,10 @@ def find_distance_between_nodes(interface, node1, node2):
 def should_send_sitrep_after_midnight():
     global last_routine_sitrep_date
     today = datetime.date.today()
+    # Check if the last routine sitrep variable is not set
+    if not last_routine_sitrep_date:
+        last_routine_sitrep_date = today
+        return True
     if last_routine_sitrep_date != today:
         last_routine_sitrep_date = today
         return True
