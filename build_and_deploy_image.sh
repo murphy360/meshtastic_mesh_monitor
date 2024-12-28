59622e8ff6bc71e539abdbf011fa1f8ba7cca0ae
Corey Murphy <c.murphy360@gmail.com>
2024-12-28 05:11:48 -0800

    Clean Code

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -22,12 +22,6 @@ printf "Pulling the latest changes from the repository...\n"
 printf "***************************************************\n\n\n"
 git pull
 
-# Checkout to the specified branch
-printf "\n\n\n***************************************************\n"
-printf "Checking out to the specified branch...\n"
-printf "***************************************************\n\n\n"
-git checkout $branch
-
 # Stop and remove the Docker container
 printf "\n\n\n***************************************************\n"
 printf "Stopping and removing the Docker container...\n"
@@ -42,7 +36,6 @@ printf "***************************************************\n\n\n"
 docker build -t $image_name .
 
 docker image ls | grep $image_name
-ls -la
 
 # Run Docker Compose in detached mode
 printf "\n\n\n***************************************************\n"
