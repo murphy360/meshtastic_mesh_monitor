ef1a3d3d06f119ace731948110a85fcda62bc21b
Corey Murphy <c.murphy360@gmail.com>
2025-02-26 03:45:24 -0800

    Add logging to serial-test.py for connection and message receipt

--- src/serial-test.py
+++ src/serial-test.py
@@ -1,22 +1,32 @@
+import logging
 import meshtastic.serial_interface
 from pubsub import pub
 import time
 
+# Configure logging
+logging.basicConfig(format='%(asctime)s - %(filename)s:%(lineno)d - %(message)s', level=logging.INFO)
+
 reply_message = "Message Received"
 
 interface = meshtastic.serial_interface.SerialInterface()
+def onConnection(interface, topic=pub.AUTO_TOPIC):
+    logging.info("Connected to Meshtastic device")
+
+    
 def onReceive(packet, interface):
+    logging.info(f"Received packet: ")
     try:
         if 'decoded' in packet and packet['decoded']['portnum'] == 'TEXT_MESSAGE_APP':
             message_bytes = packet['decoded']['payload']
             message_string = message_bytes.decode('utf-8')
-            print(f"Received: {message_string}")
+            logging.info(f"Message: {message_string}")
             send_message(reply_message)
 
     except KeyError as e:
         print(f"Error processing packet: {e}")
 
 pub.subscribe(onReceive, 'meshtastic.receive')
+pub.subscribe(onConnection, "meshtastic.connection.established")
 
 def send_message(message):
     interface.sendText(message)
