8b2cef540e92208dd28d479e61d05451559420fc
Corey Murphy <c.murphy360@gmail.com>
2025-05-13 08:01:30 -0700

    Remove Meshtastic version logging from interface status output

--- src/mesh-monitor.py
+++ src/mesh-monitor.py
@@ -861,7 +861,6 @@ while True:
                 Interface Serial Port: {serial_port}\n      \
                 Interface Node Number: {node_info['num']}\n      \
                 Interface Node Short Name: {node_info['user']['shortName']}\n      \
-                #Meshtastic Version: {meshtastic.the_version}\n      \
             **************************************************************\n    \
             **************************************************************\n\n ")
 
