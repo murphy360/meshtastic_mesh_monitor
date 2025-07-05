f7ded2477fecf07c74645a888eef2d74083ce75f
Corey Murphy <c.murphy360@gmail.com>
2025-07-05 04:24:28 -0700

    Update weather alert and forecast functions to use admin channel number instead of public channel number

--- src/mesh-monitor.py
+++ src/mesh-monitor.py
@@ -1724,10 +1724,10 @@ while True:
             continue  # Skip the rest of the loop and try to reconnect
     
         # Check for weather alerts
-        send_weather_alerts_if_needed(interface, public_channel_number)
+        send_weather_alerts_if_needed(interface, admin_channel_number)
 
         # Check if we need to send a weather forecast
-        send_weather_forecast_if_needed(interface, public_channel_number)
+        send_weather_forecast_if_needed(interface, admin_channel_number)
         
         # Send a routine sitrep every 24 hours at 00:00 UTC        
         sitrep.send_sitrep_if_new_day(interface)
