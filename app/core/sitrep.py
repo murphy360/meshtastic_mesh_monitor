093170371aa0c2ce331ccf85372b0347b127ea03
Corey Murphy <c.murphy360@gmail.com>
2025-09-21 04:56:16 -0700

    Add get_instance method to SITREP class for singleton access

--- app/core/sitrep.py
+++ app/core/sitrep.py
@@ -13,6 +13,12 @@ class SITREP:
             cls._instance = super(SITREP, cls).__new__(cls)
         return cls._instance
 
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
     def __init__(self):
         self.logger = get_logger(self.__class__.__name__)
         self.interface = None
