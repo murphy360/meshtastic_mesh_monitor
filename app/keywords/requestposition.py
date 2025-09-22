73328dfa95a40a6fc7c6bbf3e2a218c69513740e
Corey Murphy <c.murphy360@gmail.com>
2025-09-22 03:33:36 -0700

    Refactor RequestpositionKeyword to use MessageSender directly for position requests and message sending

--- app/keywords/requestposition.py
+++ app/keywords/requestposition.py
@@ -1,5 +1,4 @@
 from keywords.base import KeywordHandler
-from utils.node_info_utils import send_position_request
 from utils.message_sender import MessageSender
 from utils.node_info_utils import lookup_node
 
@@ -32,9 +31,9 @@ class RequestpositionKeyword(KeywordHandler):
             node_short_name = args[1]
             node = lookup_node(interface, node_short_name)
             if node:
-                send_position_request(interface, node['num'], channel)
+                MessageSender.send_position_request(interface, node['num'], channel)
                 reply = f"Requested position from {node_short_name}"
             else:
                 reply = f"Node {node_short_name} not found in my database. Unable to request position."
-        sender = MessageSender()
-        sender.send_message(interface, reply, channel, to_id)
+        
+        MessageSender.send_message(interface, reply, channel, to_id)
