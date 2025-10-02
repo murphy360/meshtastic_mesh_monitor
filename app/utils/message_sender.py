c0ebb61dcf6fa3453f3b334470f5966a7c786a8b
Corey Murphy <c.murphy360@gmail.com>
2025-10-02 05:39:32 -0700

    Fix encoding in send_reply method to use default UTF-8 encoding for payload.

--- app/utils/message_sender.py
+++ app/utils/message_sender.py
@@ -296,7 +296,7 @@ class MessageSender:
             # Prepare reply as a Data protobuf, ensure UTF-8 encoding and set reply_id
             from meshtastic.protobuf import mesh_pb2, portnums_pb2
             data_message = mesh_pb2.Data(
-                payload=reply_text.encode("utf-8"),
+                payload=reply_text.encode(),
                 reply_id=original_message_id
             )
             sent_packet = interface.sendData(
