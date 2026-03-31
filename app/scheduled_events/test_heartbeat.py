5e60fd7784312e67148a30558aa9dc41736a3d8d
Corey Murphy <corey.murphy@seebyte.us>
2026-03-31 14:35:16 -0700

    Enable Test Heartbeat scheduled event by setting enabled to True

--- app/scheduled_events/test_heartbeat.py
+++ app/scheduled_events/test_heartbeat.py
@@ -22,7 +22,7 @@ class TestHeartbeatScheduledEvent(BaseScheduledEvent):
     
     # Task configuration
     name = "Test Heartbeat"
-    enabled = False
+    enabled = True
     schedule_type = ScheduleType.INTERVAL
     interval_minutes = 1  # Every minute
     
